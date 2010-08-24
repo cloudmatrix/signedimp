66ed5909fcd3a834ba6dad84c72deae148c96955
Ryan Kelly <ryan@rfk.id.au>
2010-08-24 21:42:36 +1000

    take advantage of new "verify" hook for esky bootstrap code

--- signedimp/compat/esky.py
+++ signedimp/compat/esky.py
@@ -16,46 +16,16 @@ def get_bootstrap_code(key):
     signedimp_bootstrap = signedimp.tools.get_bootstrap_code(indent="    ")
     pubkey = key.get_public_key()
     return """
-def _make_signedimp_chainload(orig_chainload):
+def _make_signedimp_verify(orig_verify):
     %(signedimp_bootstrap)s
-    def _chainload(target_dir):
-        key = signedimp.%(pubkey)r
-        manager = signedimp.SignedImportManager([key])
-        manager.install()
-        #  On OSX, the signature file may be within a bundled ".app" directory
-        #  or in the top level of the target dir.
-        if sys.platform == "darwin":
-            if __esky_name__ is not None:
-                signed_dir = pathjoin(target_dir,__esky_name__+".app")
-                if not exists(pathjoin(signed_dir,signedimp.HASHFILE_NAME)):
-                    signed_dir = target_dir
-            else:
-                for nm in listdir(target_dir):
-                    if nm.endswith(".app"):
-                        signed_dir = pathjoin(target_dir,nm)
-                        if exists(pathjoin(signed_dir,signedimp.HASHFILE_NAME)):
-                            break
-                else:
-                    signed_dir = target_dir
-        else:
-            signed_dir = target_dir
-        loader = signedimp.DefaultImporter(signed_dir,signed_dir)
-        loader = signedimp.SignedLoader(manager,loader)
-        for target_exe in get_exe_locations(target_dir):
-            #  Calling get_data forces verification of the specfied data file.
-            try:
-                loader.get_data(target_exe[len(signed_dir)+1:])
-                break
-            except EnvironmentError:
-                pass
-        target_exe_dir = dirname(target_exe[len(signed_dir):])
-        for nm in listdir(dirname(target_exe)):
-            if nm.startswith("python2") or nm.startswith("python3"):
-                if nm.endswith(".dll"):
-                    loader.get_data(target_exe_dir+nm)
-        orig_chainload(target_dir)
-    return _chainload
-_chainload = _make_signedimp_chainload(_chainload)
+    key = signedimp.%(pubkey)r
+    manager = signedimp.SignedImportManager([key])
+    manager.install()
+    def verify(target_file):
+        orig_verify(target_file)
+        manager._verify_file(target_file)
+    return verify
+verify = _make_signedimp_verify(verify)
 """ % locals()
 
 
